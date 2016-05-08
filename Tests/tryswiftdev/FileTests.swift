b83564cac70d8df1ac4e5327491d659e5e91fe47
JPMartha <jpmartha.jp@gmail.com>
2016-05-09 04:48:38 +0900

    Update construction

--- Tests/tryswiftdev/FileTests.swift
+++ Tests/tryswiftdev/FileTests.swift
@@ -3,6 +3,8 @@ import XCTest
 
 private var cd = String()
 private let testDirectoryName = "tryswiftdevtest"
+private let beforeTheUpdateDirectoryName = "BeforeTheUpdate"
+private let afterTheUpdateDirectoryName = "AfterTheUpdate"
 private let configurationFileName = "testTryswiftdev.uvs"
 private let configurationFileNameForReset = "testTryswiftdev.uvsr"
 private let configurationFileNameOneLine = "testTryswiftdevOneLine.uvs"
@@ -27,7 +29,7 @@ final class FileTests: XCTestCase {
             return
         }
         
-        XCTAssertEqual(versionStringsInfos.first?.filePath, "./\(testDirectoryName)/testFindItOneLine.swift")
+        XCTAssertEqual(versionStringsInfos.first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/testFindItOneLine.swift")
         XCTAssertEqual(versionStringsInfos.first?.regularExpression, "org.swift.[0-9]\\{10\\}a")
         XCTAssertEqual(versionStringsInfos.first?.newString, "org.swift.3020171225a")
     }
@@ -39,63 +41,63 @@ final class FileTests: XCTestCase {
             return
         }
         
-        XCTAssertEqual(versionStringsInfos.first?.filePath, "./\(testDirectoryName)/testFindIt.swift")
+        XCTAssertEqual(versionStringsInfos.first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/testFindIt.swift")
         XCTAssertEqual(versionStringsInfos.first?.regularExpression, "\\\"TOOLCHAINS=org.swift.[0-9]\\{10\\}a\\\"")
         XCTAssertEqual(versionStringsInfos.first?.newString, "\\\"TOOLCHAINS=org.swift.3020171225a\\\"")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst().first?.filePath, "./\(testDirectoryName)/testFindIt.swift")
+        XCTAssertEqual(versionStringsInfos.dropFirst().first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/testFindIt.swift")
         XCTAssertEqual(versionStringsInfos.dropFirst().first?.regularExpression, "swift-DEVELOPMENT-SNAPSHOT-[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}-a")
         XCTAssertEqual(versionStringsInfos.dropFirst().first?.newString, "swift-DEVELOPMENT-SNAPSHOT-2017-12-25-a")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst(2).first?.filePath, "./\(testDirectoryName)/testFindIt.md")
+        XCTAssertEqual(versionStringsInfos.dropFirst(2).first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/testFindIt.md")
         XCTAssertEqual(versionStringsInfos.dropFirst(2).first?.regularExpression, "swift-DEVELOPMENT-SNAPSHOT-[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}-a")
         XCTAssertEqual(versionStringsInfos.dropFirst(2).first?.newString, "swift-DEVELOPMENT-SNAPSHOT-2017-12-25-a")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst(3).first?.filePath, "./\(testDirectoryName)/testFindItTests.swift")
+        XCTAssertEqual(versionStringsInfos.dropFirst(3).first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/testFindItTests.swift")
         XCTAssertEqual(versionStringsInfos.dropFirst(3).first?.regularExpression, "swift-DEVELOPMENT-SNAPSHOT-[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}-a")
         XCTAssertEqual(versionStringsInfos.dropFirst(3).first?.newString, "swift-DEVELOPMENT-SNAPSHOT-2017-12-25-a")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst(4).first?.filePath, "./\(testDirectoryName)/.travis.yml")
+        XCTAssertEqual(versionStringsInfos.dropFirst(4).first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/.travis.yml")
         XCTAssertEqual(versionStringsInfos.dropFirst(4).first?.regularExpression, "osx_image:\\ xcode[0-9].[0-9]")
         XCTAssertEqual(versionStringsInfos.dropFirst(4).first?.newString, "osx_image:\\ xcode8.0")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst(5).first?.filePath, "./\(testDirectoryName)/.travis.yml")
+        XCTAssertEqual(versionStringsInfos.dropFirst(5).first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/.travis.yml")
         XCTAssertEqual(versionStringsInfos.dropFirst(5).first?.regularExpression, "swift-DEVELOPMENT-SNAPSHOT-[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}-a")
         XCTAssertEqual(versionStringsInfos.dropFirst(5).first?.newString, "swift-DEVELOPMENT-SNAPSHOT-2017-12-25-a")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst(6).first?.filePath, "./\(testDirectoryName)/testTryswiftdev.podspec")
+        XCTAssertEqual(versionStringsInfos.dropFirst(6).first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/testTryswiftdev.podspec")
         XCTAssertEqual(versionStringsInfos.dropFirst(6).first?.regularExpression, "s.version\\ \\ \\ \\ \\ =\\ \\\"[0-9].[0-9].[0-9]\"")
         XCTAssertEqual(versionStringsInfos.dropFirst(6).first?.newString, "s.version\\ \\ \\ \\ \\ =\\ \\\"3.1.2\\\"")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst(7).first?.filePath, "./\(testDirectoryName)/testTryswiftdev.podspec")
+        XCTAssertEqual(versionStringsInfos.dropFirst(7).first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/testTryswiftdev.podspec")
         XCTAssertEqual(versionStringsInfos.dropFirst(7).first?.regularExpression, "s.osx.deployment_target\\ =\\ \\\"[0-9]\\{2\\}.[0-9]\\\"")
         XCTAssertEqual(versionStringsInfos.dropFirst(7).first?.newString, "s.osx.deployment_target\\ =\\ \\\"11.0\\\"")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst(8).first?.filePath, "./\(testDirectoryName)/testTryswiftdev.podspec")
+        XCTAssertEqual(versionStringsInfos.dropFirst(8).first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/testTryswiftdev.podspec")
         XCTAssertEqual(versionStringsInfos.dropFirst(8).first?.regularExpression, "s.ios.deployment_target\\ =\\ \\\"[0-9].[0-9]\\\"")
         XCTAssertEqual(versionStringsInfos.dropFirst(8).first?.newString, "s.ios.deployment_target =\\ \\\"9.1\\\"")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst(9).first?.filePath, "./\(testDirectoryName)/testTryswiftdev.podspec")
+        XCTAssertEqual(versionStringsInfos.dropFirst(9).first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/testTryswiftdev.podspec")
         XCTAssertEqual(versionStringsInfos.dropFirst(9).first?.regularExpression, "s.tvos.deployment_target\\ =\\ \\\"[0-9].[0-9]\"")
         XCTAssertEqual(versionStringsInfos.dropFirst(9).first?.newString, "s.tvos.deployment_target =\\ \\\"8.2\\\"")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst(10).first?.filePath, "./\(testDirectoryName)/testTryswiftdev.podspec")
+        XCTAssertEqual(versionStringsInfos.dropFirst(10).first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/testTryswiftdev.podspec")
         XCTAssertEqual(versionStringsInfos.dropFirst(10).first?.regularExpression, "s.watchos.deployment_target\\ =\\ \\\"[0-9].[0-9]\\\"")
         XCTAssertEqual(versionStringsInfos.dropFirst(10).first?.newString, "s.watchos.deployment_target\\ =\\ \\\"3.1\\\"")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst(11).first?.filePath, "./\(testDirectoryName)/Cartfile")
+        XCTAssertEqual(versionStringsInfos.dropFirst(11).first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/Cartfile")
         XCTAssertEqual(versionStringsInfos.dropFirst(11).first?.regularExpression, "github\\ \\\"ishkawa\\/APIKit\\\"\\ \\\"[0-9].[0-9].[0-9]\\\"")
         XCTAssertEqual(versionStringsInfos.dropFirst(11).first?.newString, "github\\ \\\"ishkawa\\/APIKit\\\"\\ \\\"3.4.5\\\"")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst(12).first?.filePath, "./\(testDirectoryName)/Cartfile")
+        XCTAssertEqual(versionStringsInfos.dropFirst(12).first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/Cartfile")
         XCTAssertEqual(versionStringsInfos.dropFirst(12).first?.regularExpression, "github\\ \\\"ikesyo\\/Himotoki\\\"\\ \\\"[0-9].[0-9].[0-9]\\\"")
         XCTAssertEqual(versionStringsInfos.dropFirst(12).first?.newString, "github\\ \\\"ikesyo\\/Himotoki\\\"\\ \\\"4.5.6\\\"")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst(13).first?.filePath, "./\(testDirectoryName)/Cartfile.private")
+        XCTAssertEqual(versionStringsInfos.dropFirst(13).first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/Cartfile.private")
         XCTAssertEqual(versionStringsInfos.dropFirst(13).first?.regularExpression, "github\\ \\\"jspahrsummers\\/xcconfigs\\\"\\ \\\"[a-z0-9]\\{7\\}\\\"")
         XCTAssertEqual(versionStringsInfos.dropFirst(13).first?.newString, "github\\ \\\"jspahrsummers\\/xcconfigs\\\"\\ \\\"9fe1cb3\\\"")
         
-        XCTAssertEqual(versionStringsInfos.dropFirst(14).first?.filePath, "./\(testDirectoryName)/.swift-version")
+        XCTAssertEqual(versionStringsInfos.dropFirst(14).first?.filePath, "./\(testDirectoryName)/\(afterTheUpdateDirectoryName)/.swift-version")
         XCTAssertEqual(versionStringsInfos.dropFirst(14).first?.regularExpression, "DEVELOPMENT-SNAPSHOT-[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}-a")
         XCTAssertEqual(versionStringsInfos.dropFirst(14).first?.newString, "DEVELOPMENT-SNAPSHOT-2017-12-25-a")
     }
@@ -110,7 +112,7 @@ func compareFile(path afterFilePath: String, to beforeFilePath: String) {
 }
 
 extension ReadmeTests {
-    
+    // TODO:
 }
 
 extension VersionTests {
@@ -129,8 +131,8 @@ extension VersionTests {
         ]
         
         targetFiles.forEach {
-            let targetFullPath = "\(cd)/\(testDirectoryName)/\($0)"
-            let beforeChangeFilePath = "\(cd)/\(testDirectoryName)/beforeChange/\($0)"
+            let targetFullPath = "\(cd)/\(testDirectoryName)/\(afterTheUpdateDirectoryName)/\($0)"
+            let beforeChangeFilePath = "\(cd)/\(testDirectoryName)/\(beforeTheUpdateDirectoryName)/\($0)"
             // TODO: How to test
             compareFile(path: targetFullPath, to: beforeChangeFilePath)
         }
@@ -151,8 +153,8 @@ extension VersionTests {
         ]
         
         targetFiles.forEach {
-            let targetFullPath = "\(cd)/\(testDirectoryName)/\($0)"
-            let beforeChangeFilePath = "\(cd)/\(testDirectoryName)/beforeChange/\($0)"
+            let targetFullPath = "\(cd)/\(testDirectoryName)/\(afterTheUpdateDirectoryName)/\($0)"
+            let beforeChangeFilePath = "\(cd)/\(testDirectoryName)/\(beforeTheUpdateDirectoryName)/\($0)"
             compareFile(path: targetFullPath, to: beforeChangeFilePath)
         }
     }
